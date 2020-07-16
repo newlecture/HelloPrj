8803bc0307f9a4f9304fbea572675b0fc7747d91
narafu <narafu@kakao.com>
2020-07-16 16:06:08 +0900

    update program(total)

--- src/Program.java
+++ src/Program.java
@@ -6,13 +6,15 @@ public class Program {
 		System.out.println("Hello Java");
 		int x = 3;
 		int y = 4;
-		int total = x + y;
-		System.out.printf("total is %d\n", total);
-		System.out.printf("avg is %f\n", total/3.0f);
+
 		// line 1
 		// line 2
 		// new branch line1
 		// new branch line2
+		int z = 100;
+		int total = x + y + z;
+		System.out.printf("total is %d\n", total);
+		System.out.printf("avg is %f\n", total/3.0f);
 	}
 
 }
